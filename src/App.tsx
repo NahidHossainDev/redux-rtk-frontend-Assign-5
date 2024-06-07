9b4c2b6ed58f69b4174d6a7919684000abbf772b
NahidHossainDev <nahidhossain.dev@gmail.com>
2024-06-07 16:08:11 +0600

    firebase login persistence implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { auth } from './lib/firebase';
+import {
+  setLoading,
+  setUserAuthentication,
+  updatedUser,
+} from './redux/features/user/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading(true));
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUserAuthentication(true));
+        dispatch(updatedUser(user));
+        dispatch(setLoading(false));
+      } else {
+        dispatch(setLoading(false));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <Toaster />
